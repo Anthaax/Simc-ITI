305b230d8cc7c933087218a55f839039c58e904d
Anthaax <guillaume.fimes@laposte.net>
2015-05-05 09:26:04 +0200

    Version guillaume avec argent

---
+++ Simc-ITI/ITI.Simc-ITI.Money.Lib/Money.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.Simc_ITI.Money.Lib
+{
+    public class Money
+    {
+        int _myMoney;
+        public Money()
+        {
+            _myMoney = 5000;
+        }
+
+        public int MyMoney
+        {
+            get { return _myMoney; }
+            set { _myMoney = value;}
+        }
+
+        public bool CanBuid(int price)
+        {
+            int M = MyMoney;
+            if( M + 1 >= price )
+            {
+                BuyBuilding( price);
+                return true;
+            }
+            return false;
+        }
+
+        private void BuyBuilding( int price )
+        {
+            int newMoney = _myMoney - price;
+            MyMoney = newMoney;
+        }
+    }
+}
