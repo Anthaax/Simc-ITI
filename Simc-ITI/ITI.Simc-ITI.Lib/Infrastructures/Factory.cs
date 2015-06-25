d3aa27efddd362f4b8a73d1ec6602310d261d920
Anthaax <guillaume.fimes@laposte.net>
2015-06-25 12:21:11 +0200

    Petit changement

--- Simc-ITI/ITI.Simc-ITI.Lib/Infrastructures/Factory.cs
+++ Simc-ITI/ITI.Simc-ITI.Lib/Infrastructures/Factory.cs
@@ -80,7 +80,8 @@ namespace ITI.Simc_ITI.Build
         public int Taxation { get { return _taxation; } set { _taxation = value; } }
         public int Salary { get { return _salary; } }
         public int FireChance { get { return _fireChance; } set { _fireChance = value; } }
-        public event EventHandler IsOnFire;[NonSerialized]
+        [NonSerialized]
+        public event EventHandler IsOnFire;
         public bool IsBurnig
         {
             get { return _isBurning; }
